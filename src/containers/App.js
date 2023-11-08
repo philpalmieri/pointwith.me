4a21f7a045874f32caf8f0152df9111d79ebb1e9
Keith Baker <github@baker.af>
2023-11-08 14:24:45 -0500

    update React-Router deprecations

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Login from '../components/Login';
 import Dashboard from '../components/Dashboard';
@@ -8,22 +8,22 @@ import About from '../components/About';
 import withAuthentication from '../containers/withAuthentication';
 import '../style.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+    console.group('App');
+    console.groupEnd();
     return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/dashboard" component={withAuthentication(Dashboard)} />
-          <Route
-            path="/table/:userId/:tableId"
-            component={withAuthentication(PokerTable)}
-          />
-          <Route path="/about" component={About} />
-        </Switch>
-      </Router>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Login />}/>
+                <Route path="/dashboard" element={withAuthentication(Dashboard)}/>
+                <Route
+                    path="/table/:userId/:tableId"
+                    element={withAuthentication(PokerTable)}
+                />
+                <Route path="/about" element={<About />}/>
+            </Routes>
+        </BrowserRouter>
     );
-  }
 }
 
 export default App;
